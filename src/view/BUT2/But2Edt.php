5eec50e078ba6a7099f6acd1a54068a6a07519fb
SoudaniLyes <lyes-yacine.SOUDANI-REGUIG@etu.univ-amu.fr>
2023-12-21 11:14:19 +0100

    commit

---
+++ src/view/BUT2/But2Edt.php
@@ -0,0 +1,24 @@
+<!--Vue total Du but Avec choix de voir l'annnée ou les groupes*/-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>EDT BUT2</title>
+    <link rel="icon" type="image/png" href="../../_assets/images/favicon.png">
+    <link rel="stylesheet" href="../../_assets/styles/But2EdtStyle.css">
+    <link rel="stylesheet" href="../../_assets/styles/PageCommune.css">
+</head>
+<body>
+<div class="container">
+    <img id="image_up" src="../../_assets/images/BUT2_Up.png">
+</div>
+<a  href="But2.html" >
+    <img id="return" src="../../_assets/images/return.png">
+</a>
+<img id="logo" src="../../_assets/images/logo.png" alt="Logo">
+<img id="GA1" src="../../_assets/images/GA1.png">
+<img id="GA2" src="../../_assets/images/A2.png">
+<img id="GB" src="../../_assets/images/GB.png">
+</body>
+</html>
