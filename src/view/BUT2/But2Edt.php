593fafe0a98e627dba72e9f43430e24abf3350fa
Mohamed MIESLI <mohamed.mesli@etu.univ-amu.fr>
2023-12-25 14:06:00 +0100

    Correction des erreurs principale

--- src/view/BUT2/But2Edt.php
+++ src/view/BUT2/But2Edt.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>EDT BUT2</title>
     <link rel="icon" type="image/png" href="../../../_assets/images/favicon.png">
-    <link rel="stylesheet" href="../../../_assets/styles/But2EdtStyle.css">
+    <link rel="stylesheet" href="../../../_assets/styles/But2Annee.css">
     <link rel="stylesheet" href="../../../_assets/styles/PageCommune.css">
 </head>
 <body>
@@ -16,7 +16,7 @@
     <img id="GA1" src="../../../_assets/images/B2GA2.png">
 </a>
 <a href="index.php?action=BUT2ga2">
-    <img id="GA2" src="../../../_assets/images/B2GA2.png">
+    <img id="GA2" src="../../../_assets/images/B2GA1.png">
 </a>
 <a href="index.php?action=BUT2gb">
     <img id="GB" src="../../../_assets/images/B2GB.png">
@@ -26,4 +26,4 @@
 </a>
 <img id="logo" src="../../../_assets/images/logo.png" alt="Logo">
 </body>
-</html>ç
+</html>
