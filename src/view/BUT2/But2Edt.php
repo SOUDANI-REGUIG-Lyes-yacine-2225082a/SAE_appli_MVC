0ddcaa9a2bbb4a04f85216b8836d5ee27a083624
Mohamed MIESLI <mohamed.mesli@etu.univ-amu.fr>
2023-12-25 12:09:42 +0100

    wsh

--- src/view/BUT2/But2Edt.php
+++ src/view/BUT2/But2Edt.php
@@ -1,4 +1,3 @@
-<!-- BUT 2 Annee en entier -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,13 +5,15 @@
     <meta charset="UTF-8">
     <title>EDT BUT2</title>
     <link rel="icon" type="image/png" href="../../../_assets/images/favicon.png">
-    <link rel="stylesheet" href="../../../_assets/styles/But2EdtStyle.css">
+    <link rel="stylesheet" href="../../../../_assets/styles/But2EdtStyle.css">
     <link rel="stylesheet" href="../../../_assets/styles/PageCommune.css">
 </head>
 <body>
+<a href="index.php?action=But2Edt">
 <img id="image_up" src="../../../_assets/images/BUT1_Up.png">
+</a>
 <a href="index.php?action=BUT2ga1">
-    <img id="GA1" src="../../../_assets/images/B2GA1.png">
+    <img id="GA1" src="../../../_assets/images/B2GA2.png">
 </a>
 <a href="index.php?action=BUT2ga2">
     <img id="GA2" src="../../../_assets/images/B2GA2.png">
@@ -22,7 +23,7 @@
 </a>
 <a  href="index.php?action=roueDeChoix" >
     <img id="return" src="../../../_assets/images/return.png">
-</a
+</a>
 <img id="logo" src="../../../_assets/images/logo.png" alt="Logo">
 </body>
 </html>ç
