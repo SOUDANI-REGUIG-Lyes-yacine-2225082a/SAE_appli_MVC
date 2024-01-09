4a5b6dcfbf8a1862042f23c8fc3bd9261d75407b
MESLIMOHAMEDM22005188 <mohamed.mesli@etu.univ-amu.fr>
2024-01-09 08:31:01 +0100

    correction image Enseignat

--- src/view/choixRoueView.php
+++ src/view/choixRoueView.php
@@ -21,8 +21,8 @@
     <a href="index.php?group=but2">
         <img id="BUT2" src="../../_assets/images/But2.png" width="145" height="130">
     </a>
-    <a href="index.php?group=Enseignant">
-    <img id="ButEnseignant" src="../../_assets/images/enseignant.png" width="125" height="120">
+    <a href="index.php?group=ButEnseignant">
+        <img id="ButEnseignant" src="../../_assets/images/enseignant.png" width="125" height="120">
     </a>
     <a href="index.php?group=salles">
         <img id="SallesInformatique" src="../../_assets/images/SallesInformatique.png" width="145" height="135">
