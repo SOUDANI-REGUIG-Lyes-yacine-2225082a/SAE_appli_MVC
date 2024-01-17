41f5ddb14a4a929ba1c1ebf5417d943ae305967f
Audran <115973951+SANTOS-Audran-2225078a@users.noreply.github.com>
2024-01-17 20:11:31 +0100

    Add files via upload

--- test/ProfModelTest.php
+++ test/ProfModelTest.php
@@ -1,29 +1,28 @@
 <?php
 
 namespace src\controller;
-use PHPUnit\Framework\TestCase;
-use src\model\EventModel;
-class EventModelTest
+
+class ProfModelTest
 {
+    public function testSupprimerProfesseur() {
+        $model = new ProfModel();
+        $model->supprimerProfesseur('NomProf');
+    }
     public function testGenerateIcsUrl() {
-        $model = new EventModel();
-        $url = $model->generateIcsUrl('BUT1g1', '2024-01-01', '2024-01-07');
+        $model = new ProfModel();
+        $url = $model->generateIcsUrl('NomGroupe', '2024-01-01', '2024-01-07');
         $this->assertStringContainsString('2024-01-01', $url);
         $this->assertStringContainsString('2024-01-07', $url);
     }
     public function testPrepareData() {
-        $model = new EventModel();
+        $model = new ProfModel();
         $cleanedData = $model->prepareData("Some\nText\nWith\nNewlines");
         $this->assertEquals("SomeTextWithNewlines", $cleanedData);
     }
     public function testRetrieveIcs() {
-        $model = $this->createMock(EventModel::class);
+        $model = $this->createMock(ProfModel::class);
         $model->method('recupIcs')->willReturn(/* Simuler la réponse attendue */);
-        $events = $model->retrieveIcs('BUT1g1', '2024-01-01', '2024-01-07');
-    }
-    public function testGetAvailableRooms() {
-        $model = new EventModel();
-        $availableRooms = $model->getAvailableRooms();
+        $events = $model->retrieveIcs('NomGroupe', '2024-01-01', '2024-01-07');
     }
 
 }
